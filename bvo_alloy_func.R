4646231d58bdb9abc7e59fa09d57564acc9cd0c6
Dan <guevarra@caltech.edu>
2017-05-25 15:48:09 -0700

    maxy and miny parameters for defining Y scales

--- bvo_alloy_func.R
+++ bvo_alloy_func.R
@@ -1,4 +1,17 @@
-alloyplots<-function(fomdt, alloys=c('C', 'D', 'E', 'F', 'G', 'H'), titlestring='labelme') {
+alloyplots<-function(fomdt, alloys=c('C', 'D', 'E', 'F', 'G', 'H'), fom='pmax', titlestring='labelme', aggmethod='', yscale=1E3/0.0058, maxy=1, miny=0, plot=T) {
+    
+    if(aggmethod=='top2') {
+        aggfunc={function(x) mean(na.omit(sort(x,decreasing=T)[1:2]))}
+    }
+    else if (aggmethod=='mymed'){
+        aggfunc={function(x) if(length(x)>1) na.omit(sort(x, decreasing=T))[floor(length(x)/2)] else x}
+    }
+    else {
+        aggfunc={function(x) median(na.omit(x))}
+    }
+    
+    usefom<-parse(text=fom)
+        
     pm<-getpm(72)
     pm[,`:=`(Bi=A*0.156, V=(A*0.144+B*0.11), C_=C*0.022, D_=D*0.022, E_=E*0.022, F_=F*0.022, G_=G*0.022, H_=H*0.022)]
     pm[,alloy_sum:=C_+D_+E_+F_+G_+H_]
@@ -7,13 +20,13 @@ alloyplots<-function(fomdt, alloys=c('C', 'D', 'E', 'F', 'G', 'H'), titlestring=
     setkey(pm, Sample)
 
     # single alloys
-    sal<-pm[fomdt][idx==2][A+B>0][D+E+F+G+H==0 | C+E+F+G+H==0 | C+D+F+G+H==0 | C+D+E+G+H==0 | C+D+E+F+H==0 | C+D+E+F+G==0]
-    salm<-melt(sal[!is.na(pmax)][,.(pmed=median(pmax, na.rm=T), jmed=median(jsc, na.rm=T)), by=list(Bi, V, C_, D_, E_, F_, G_, H_, alloy_sum, BiV_ratio)], id.vars=c('Bi', 'V', 'pmed', 'jmed', 'alloy_sum', 'BiV_ratio'))
+    sal<-pm[fomdt][A+B>0][D+E+F+G+H==0 | C+E+F+G+H==0 | C+D+F+G+H==0 | C+D+E+G+H==0 | C+D+E+F+H==0 | C+D+E+F+G==0]
+    salm<-melt(sal[!is.na(eval(usefom))][,.(pmed=aggfunc(eval(usefom)), jmed=aggfunc(jsc)), by=list(Bi, V, C_, D_, E_, F_, G_, H_, alloy_sum, BiV_ratio)], id.vars=c('Bi', 'V', 'pmed', 'jmed', 'alloy_sum', 'BiV_ratio'))
     salm[,variable:=factor(variable, levels=c('C_', 'D_', 'E_', 'F_', 'G_', 'H_'), labels=alloys)]
 
     # pures
-    pur<-pm[fomdt][idx==2][A+B>0][C+D+E+F+G+H==0]
-    purm<-melt(pur[!is.na(pmax)][,.(pmed=median(pmax, na.rm=T), jmed=median(jsc, na.rm=T)), by=list(Bi, V, C_, D_, E_, F_, G_, H_, alloy_sum, BiV_ratio)], id.vars=c('Bi', 'V', 'pmed', 'jmed', 'alloy_sum', 'BiV_ratio'))
+    pur<-pm[fomdt][A+B>0][C+D+E+F+G+H==0]
+    purm<-melt(pur[!is.na(eval(usefom))][,.(pmed=aggfunc(eval(usefom)), jmed=aggfunc(jsc)), by=list(Bi, V, C_, D_, E_, F_, G_, H_, alloy_sum, BiV_ratio)], id.vars=c('Bi', 'V', 'pmed', 'jmed', 'alloy_sum', 'BiV_ratio'))
     purm[,variable:=factor(variable, levels=c('C_', 'D_', 'E_', 'F_', 'G_', 'H_'), labels=alloys)]
 
     # merge pure and single alloys
@@ -22,56 +35,56 @@ alloyplots<-function(fomdt, alloys=c('C', 'D', 'E', 'F', 'G', 'H'), titlestring=
 
     # group into binaries
 
-    cgrp<-pm[fomdt][idx==2][A+B>0][(C+D>0 & E+F+G+H==0) |
+    cgrp<-pm[fomdt][A+B>0][(C+D>0 & E+F+G+H==0) |
                                        (C+E>0 & D+F+G+H==0) |
                                        (C+F>0 & D+E+G+H==0) |
                                        (C+G>0 & D+E+F+H==0) |
                                        (C+H>0 & D+E+F+G==0)]
 
-    dgrp<-pm[fomdt][idx==2][A+B>0][(D+C>0 & E+F+G+H==0) |
+    dgrp<-pm[fomdt][A+B>0][(D+C>0 & E+F+G+H==0) |
                                        (D+E>0 & C+F+G+H==0) |
                                        (D+F>0 & C+E+G+H==0) |
                                        (D+G>0 & C+E+F+H==0) |
                                        (D+H>0 & C+E+F+G==0)]
 
-    egrp<-pm[fomdt][idx==2][A+B>0][(E+C>0 & D+F+G+H==0) |
+    egrp<-pm[fomdt][A+B>0][(E+C>0 & D+F+G+H==0) |
                                        (E+D>0 & C+F+G+H==0) |
                                        (E+F>0 & C+D+G+H==0) |
                                        (E+G>0 & C+D+F+H==0) |
                                        (E+H>0 & C+D+F+G==0)]
 
-    fgrp<-pm[fomdt][idx==2][A+B>0][(F+C>0 & D+E+G+H==0) |
+    fgrp<-pm[fomdt][A+B>0][(F+C>0 & D+E+G+H==0) |
                                        (F+D>0 & C+E+G+H==0) |
                                        (F+E>0 & C+D+G+H==0) |
                                        (F+G>0 & C+D+E+H==0) |
                                        (F+H>0 & C+D+E+G==0)]
 
-    ggrp<-pm[fomdt][idx==2][A+B>0][(G+C>0 & D+E+F+H==0) |
+    ggrp<-pm[fomdt][A+B>0][(G+C>0 & D+E+F+H==0) |
                                        (G+D>0 & C+E+F+H==0) |
                                        (G+E>0 & C+D+F+H==0) |
                                        (G+F>0 & C+D+E+H==0) |
                                        (G+H>0 & C+D+E+F==0)]
 
-    hgrp<-pm[fomdt][idx==2][A+B>0][(H+C>0 & D+E+F+G==0) |
+    hgrp<-pm[fomdt][A+B>0][(H+C>0 & D+E+F+G==0) |
                                        (H+D>0 & C+E+F+G==0) |
                                        (H+E>0 & C+D+F+G==0) |
                                        (H+F>0 & C+D+E+G==0) |
                                        (H+G>0 & C+D+E+F==0)]
 
     # calculate binary fractions per group
-    cgrp[,alloy_frac:=C_/alloy_sum]
-    dgrp[,alloy_frac:=D_/alloy_sum]
-    egrp[,alloy_frac:=E_/alloy_sum]
-    fgrp[,alloy_frac:=F_/alloy_sum]
-    ggrp[,alloy_frac:=G_/alloy_sum]
-    hgrp[,alloy_frac:=H_/alloy_sum]
+    cgrp[,alloy_frac:=1-C_/alloy_sum]
+    dgrp[,alloy_frac:=1-D_/alloy_sum]
+    egrp[,alloy_frac:=1-E_/alloy_sum]
+    fgrp[,alloy_frac:=1-F_/alloy_sum]
+    ggrp[,alloy_frac:=1-G_/alloy_sum]
+    hgrp[,alloy_frac:=1-H_/alloy_sum]
 
-    cmelt<-melt(cgrp[!is.na(pmax)][,.(pmed=median(pmax, na.rm=T), jmed=median(jsc, na.rm=T)), by=list(Bi, V, C_, D_, E_, F_, G_, H_, alloy_sum, alloy_frac, BiV_ratio)], id.vars=c('Bi', 'V', 'pmed', 'jmed', 'alloy_sum', 'alloy_frac', 'BiV_ratio'))
-    dmelt<-melt(dgrp[!is.na(pmax)][,.(pmed=median(pmax, na.rm=T), jmed=median(jsc, na.rm=T)), by=list(Bi, V, C_, D_, E_, F_, G_, H_, alloy_sum, alloy_frac, BiV_ratio)], id.vars=c('Bi', 'V', 'pmed', 'jmed', 'alloy_sum', 'alloy_frac', 'BiV_ratio'))
-    emelt<-melt(egrp[!is.na(pmax)][,.(pmed=median(pmax, na.rm=T), jmed=median(jsc, na.rm=T)), by=list(Bi, V, C_, D_, E_, F_, G_, H_, alloy_sum, alloy_frac, BiV_ratio)], id.vars=c('Bi', 'V', 'pmed', 'jmed', 'alloy_sum', 'alloy_frac', 'BiV_ratio'))
-    fmelt<-melt(fgrp[!is.na(pmax)][,.(pmed=median(pmax, na.rm=T), jmed=median(jsc, na.rm=T)), by=list(Bi, V, C_, D_, E_, F_, G_, H_, alloy_sum, alloy_frac, BiV_ratio)], id.vars=c('Bi', 'V', 'pmed', 'jmed', 'alloy_sum', 'alloy_frac', 'BiV_ratio'))
-    gmelt<-melt(ggrp[!is.na(pmax)][,.(pmed=median(pmax, na.rm=T), jmed=median(jsc, na.rm=T)), by=list(Bi, V, C_, D_, E_, F_, G_, H_, alloy_sum, alloy_frac, BiV_ratio)], id.vars=c('Bi', 'V', 'pmed', 'jmed', 'alloy_sum', 'alloy_frac', 'BiV_ratio'))
-    hmelt<-melt(hgrp[!is.na(pmax)][,.(pmed=median(pmax, na.rm=T), jmed=median(jsc, na.rm=T)), by=list(Bi, V, C_, D_, E_, F_, G_, H_, alloy_sum, alloy_frac, BiV_ratio)], id.vars=c('Bi', 'V', 'pmed', 'jmed', 'alloy_sum', 'alloy_frac', 'BiV_ratio'))
+    cmelt<-melt(cgrp[!is.na(eval(usefom))][,.(pmed=aggfunc(eval(usefom)), jmed=aggfunc(jsc)), by=list(Bi, V, C_, D_, E_, F_, G_, H_, alloy_sum, alloy_frac, BiV_ratio)], id.vars=c('Bi', 'V', 'pmed', 'jmed', 'alloy_sum', 'alloy_frac', 'BiV_ratio'))
+    dmelt<-melt(dgrp[!is.na(eval(usefom))][,.(pmed=aggfunc(eval(usefom)), jmed=aggfunc(jsc)), by=list(Bi, V, C_, D_, E_, F_, G_, H_, alloy_sum, alloy_frac, BiV_ratio)], id.vars=c('Bi', 'V', 'pmed', 'jmed', 'alloy_sum', 'alloy_frac', 'BiV_ratio'))
+    emelt<-melt(egrp[!is.na(eval(usefom))][,.(pmed=aggfunc(eval(usefom)), jmed=aggfunc(jsc)), by=list(Bi, V, C_, D_, E_, F_, G_, H_, alloy_sum, alloy_frac, BiV_ratio)], id.vars=c('Bi', 'V', 'pmed', 'jmed', 'alloy_sum', 'alloy_frac', 'BiV_ratio'))
+    fmelt<-melt(fgrp[!is.na(eval(usefom))][,.(pmed=aggfunc(eval(usefom)), jmed=aggfunc(jsc)), by=list(Bi, V, C_, D_, E_, F_, G_, H_, alloy_sum, alloy_frac, BiV_ratio)], id.vars=c('Bi', 'V', 'pmed', 'jmed', 'alloy_sum', 'alloy_frac', 'BiV_ratio'))
+    gmelt<-melt(ggrp[!is.na(eval(usefom))][,.(pmed=aggfunc(eval(usefom)), jmed=aggfunc(jsc)), by=list(Bi, V, C_, D_, E_, F_, G_, H_, alloy_sum, alloy_frac, BiV_ratio)], id.vars=c('Bi', 'V', 'pmed', 'jmed', 'alloy_sum', 'alloy_frac', 'BiV_ratio'))
+    hmelt<-melt(hgrp[!is.na(eval(usefom))][,.(pmed=aggfunc(eval(usefom)), jmed=aggfunc(jsc)), by=list(Bi, V, C_, D_, E_, F_, G_, H_, alloy_sum, alloy_frac, BiV_ratio)], id.vars=c('Bi', 'V', 'pmed', 'jmed', 'alloy_sum', 'alloy_frac', 'BiV_ratio'))
 
     # apply alloy labels
     cmelt[,alloy:=alloys[1]]
@@ -143,25 +156,43 @@ alloyplots<-function(fomdt, alloys=c('C', 'D', 'E', 'F', 'G', 'H'), titlestring=
     
     # setup aesthetics and facet labels
     
+    biv_labels = c('x = 0.48', 'x = 0.5 (0.498 and 0.500)', 'x = 0.52 (0.519 and 0.524)')
+        
     bsub<-bsal[alloy_sum %in% c(0.0022, 0.0044, 0.0066, 0.0110)]
     bsub[,sum_alloys:=factor(alloy_sum)]
     bsub[,Alab:=factor(alloy, levels=levels(factor(alloy)), labels=paste('A =', levels(factor(alloy))))]
     bsub[,vfrac:=round(V/(Bi+V),2)]
     bsub[,Yval:=floor(100*alloy_sum/(Bi+V+alloy_sum))/100]
-    bsub[,vx_lab:=factor(vfrac, levels=levels(factor(vfrac)), labels=paste('x =', levels(factor(vfrac))))]
+    bsub[,vx_lab:=factor(vfrac, levels=levels(factor(vfrac)), labels=biv_labels)]
+    #bsub[,vx_lab:=factor(vfrac, levels=levels(factor(vfrac)), labels=paste('x =', levels(factor(vfrac))))]
     psal[,vfrac:=round(V/(Bi+V),2)]
     psal[,Yval:=alloy_sum/(Bi+V+alloy_sum)]
-    psal[,vx_lab:=factor(vfrac, levels=levels(factor(vfrac)), labels=paste('x =', levels(factor(vfrac))))]
+    psal[,vx_lab:=factor(vfrac, levels=levels(factor(vfrac)), labels=biv_labels)]
+    #psal[,vx_lab:=factor(vfrac, levels=levels(factor(vfrac)), labels=paste('x =', levels(factor(vfrac))))]
 
     # make some plots
-    splot<-ggplot(psal, aes(x=Yval, y=pmed*1E5, color=variable)) +
+    if(fom=='pmax'){
+        y_label<-expression(P[max]~(mW/cm^2))
+    }
+    else if(fom=='jsc'){
+        y_label<-expression(J[sc]~(mA/cm^2))
+    }
+    else if(fom=='voc'){
+        y_label<-expression(E[oc]~(V))
+    }
+    else {
+        y_label<-usefom
+    }
+            
+        
+    splot<-ggplot(psal, aes(x=Yval, y=pmed*yscale, color=variable)) +
         theme_bw() +
         geom_line(aes(group=variable)) +
         geom_point() +
         facet_grid(alloy~vx_lab, switch='y') +
         xlab(expression(y~'in'~Bi[1-x]*V[x]*O[4+delta]:A[y])) +
-        ylab(expression(P[max]~(mW/cm^2))) +
-        scale_y_continuous(breaks=seq(0, 1, 0.2), limits=c(0., 1)) +
+        ylab(y_label) +
+        scale_y_continuous(breaks=seq(miny, maxy, length.out=5), limits=c(miny, maxy)) +
         scale_color_manual(name=expression(A), values=gg.colors(6)) +
         theme(legend.position='right',
               strip.background=element_blank(),
@@ -169,14 +200,14 @@ alloyplots<-function(fomdt, alloys=c('C', 'D', 'E', 'F', 'G', 'H'), titlestring=
               panel.spacing=unit(0.8, 'lines')) +
         ggtitle(titlestring)
 
-    bplot<-ggplot(bsub, aes(x=alloy_frac, y=pmed*1E5, color=variable, shape=factor(Yval))) +
+    bplot<-ggplot(bsub, aes(x=alloy_frac, y=pmed*yscale, color=variable, shape=factor(Yval))) +
         theme_bw() +    
         geom_point() +
         geom_line(aes(group=interaction(variable, factor(floor(Yval*100))))) +
         facet_grid(Alab~vx_lab, switch='y') +
         xlab(expression(z~'in'~Bi[1-x]*V[x]*O[4+delta]:(A[1-z]*B[z])[y])) +
-        ylab(expression(P[max]~(W))) +
-        scale_y_continuous(breaks=seq(0, 1, 0.2), limits=c(0., 1)) +
+        ylab(y_label) +
+        scale_y_continuous(breaks=seq(miny, maxy, length.out=5), limits=c(miny, maxy)) +
         scale_shape_manual(name=expression(y), values=1:4) +
         scale_color_manual(name=expression(B), values=gg.colors(6)) +
         theme(legend.position='right',
@@ -192,5 +223,10 @@ alloyplots<-function(fomdt, alloys=c('C', 'D', 'E', 'F', 'G', 'H'), titlestring=
 
     g<-rbind(sgrob, bgrob, size='first')
     g$widths<-unit.pmax(sgrob$widths, bgrob$widths)
-    return(g)
+    if(plot) {
+        return(g)
+    }
+    else {
+        return(list(psal, bsub))
+    }
 }
